3fa323f14002a9b819f197ddcaf7b5a2966cf130
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-02-17 00:23:11 +0100

    test full chain

--- triangle_mesh_io/tests/test_convert.py
+++ triangle_mesh_io/tests/test_convert.py
@@ -1,4 +1,8 @@
 import triangle_mesh_io as tmi
+from importlib import resources as importlib_resources
+import os
+import numpy as np
+import tempfile
 
 
 def test_convert_off_to_obj():
@@ -20,3 +24,26 @@ def test_convert_stl_to_obj():
     assert len(stl_cube) == sum(
         [len(obj_cube["mtl"][mkey]) for mkey in obj_cube["mtl"]]
     )
+
+
+RESOURCE_PATH = os.path.join(
+    importlib_resources.files("triangle_mesh_io"), "tests", "resources"
+)
+
+
+def test_full_cahin():
+    NAME = "openucci-rim-disk"
+
+    with open(os.path.join(RESOURCE_PATH, NAME + ".off"), "rt") as f:
+        off = tmi.off.loads(f.read())
+
+    obj = tmi.convert.off_to_obj(
+        off=off,
+        mtl="rim",
+        vertex_eps=None,
+        vertex_normal_eps=np.deg2rad(1e-3),
+        vertex_normal_smooth_eps=np.deg2rad(10.0),
+    )
+
+    with open(os.path.join(RESOURCE_PATH, NAME + ".obj"), "wt") as f:
+        f.write(tmi.obj.dumps(obj))
